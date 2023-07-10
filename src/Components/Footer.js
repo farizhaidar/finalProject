01bd4e2edba74675352bc7c147ca198d6b91fade
farizhaidar <farizhaidar00@gmail.com>
2023-07-10 11:48:24 +0700

    about

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className='bg-gray-300 h-30 buttom-0'>
+                <div className='flex pl-8 pt-2'>
+                  <h1 className='text-white font-bold text-4xl'>TIK</h1>
+                  <h1 className='text-sky-700 font-bold pt-4'>REX</h1>
+                </div>
+
+                
+                <h1 className='text-center pb-4 pt-4'>Copyright ©2023</h1>
+          </div>
+    );
+  }
+  
+  export default Footer;
--- src/pages/TikTalks.js
+++ src/pages/TikTalks.js
@@ -57,7 +57,7 @@ const TikTalks = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 flex-1/2" >
+    <div className="container mx-auto p-4 flex-1/2 h-screen" >
       <h1 className="text-2xl font-bold mb-4">TikTalks</h1>
 
       {/* Formulir untuk posting baru */}
